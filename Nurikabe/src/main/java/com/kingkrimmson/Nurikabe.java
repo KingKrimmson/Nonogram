c219c4c907b51fe8879d1f528353637c7acbd93a
KingKrimmson <kingkrimmson@gmail.com>
2016-02-07 23:48:27 -0500

    Print Board

--- Nurikabe/src/main/java/com/kingkrimmson/Nurikabe.java
+++ Nurikabe/src/main/java/com/kingkrimmson/Nurikabe.java
@@ -12,24 +12,30 @@ import java.util.Scanner;
 public class Nurikabe {
 
     private int width, height;
-    public ArrayList<ArrayList<Integer>> x, y;
+    public ArrayList<ArrayList<String>> x, y;
     private ArrayList<ArrayList<Boolean>> board;
 
     public Nurikabe(File boardFile) throws FileNotFoundException {
         Scanner scanner = new Scanner(boardFile);
 
-        x = new ArrayList<ArrayList<Integer>>();
-        y = new ArrayList<ArrayList<Integer>>();
+        x = new ArrayList<ArrayList<String>>();
+        y = new ArrayList<ArrayList<String>>();
+
+        x.add(new ArrayList<String>());
 
         width = scanner.nextInt();
         height = scanner.nextInt();
 
+        //Dump blank line
+        scanner.nextLine();
+
         for (int w = 0; w < width; w++) {
-            x.add(new ArrayList(Arrays.asList(scanner.next().split(" "))));
+            x.add(new ArrayList(Arrays.asList(scanner.nextLine().split(" "))));
         }
         for (int h = 0; h < height; h++) {
-            y.add(new ArrayList(Arrays.asList(scanner.next().split(" "))));
+            y.add(new ArrayList(Arrays.asList(scanner.nextLine().split(" "))));
         }
+
         board = new ArrayList<ArrayList<Boolean>>();
         for (int w = 0; w < width; w++) {
             ArrayList<Boolean> row = new ArrayList<Boolean>();
@@ -40,9 +46,48 @@ public class Nurikabe {
         }
     }
 
+    public void printBoard() {
+        int maxheight=0,maxwidth=0;
+        for (ArrayList<String> row : x) {
+            if (row.size()>maxheight) {
+                maxheight = row.size();
+            }
+        }
+        for (ArrayList<String> row : y) {
+            if (row.size()>maxwidth) {
+                maxwidth = row.size();
+            }
+        }
+        for (int h = maxheight; h >= 0; h--) {
+            for (int w = maxwidth; w > 0; w-- ){
+                System.out.print("   ");
+            }
+            for (int w = 0; w < width; w++) {
+                String cell = " ";
+                try {
+                    cell = x.get(w).get(h);
+                } catch (Exception e) {}
+                System.out.print(" " + cell + " ");
+            }
+            System.out.println();
+        }
+        for (int h = 0; h < height; h++) {
+            for (int w = maxwidth; w >= 0; w--) {
+                String cell = " ";
+                try {
+                    cell = y.get(h).get(w);
+                } catch (Exception e) {}
+                System.out.print(" " + cell + " ");
+            }
+            
+            System.out.println();
+        }
+    }
+
     public static void main(String args[]){
         try {
-            Nurikabe game = new Nurikabe(new File(args[0]));
+            Nurikabe game = new Nurikabe(new File(args[0] + "/Puzzles/basic_test_2.txt"));
+            game.printBoard();
         } catch (Exception e) {
             e.printStackTrace();
         }
